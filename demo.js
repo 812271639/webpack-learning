50038e7e977dd93895b0cbfb229e61dbf9171513
812271639 <812271639@qq.com>
2018-03-18 20:45:11 +0800

    demo

--- demo.js
+++ demo.js
@@ -1,6 +1,13 @@
-const demo = {
-    namev: '小姐姐',
+var demo = {
+    name: '小姐姐',
     age: 23,
-    namev: '小姐姐',
-    age: 23
+    name1: '小姐姐',
+    age1: 23,
+    name2: '小姐姐',
+    age3: 23,
+    name3: '小姐姐',
+    age4: 23,
+    age5: 23,
+    name6: '小姐姐'
+
 }
